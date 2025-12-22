e4771e0326c2de5e2c6c115705426c10ec0c2519
Ruwan Thalagaha Gedara <ruwantgs@gmail.com>
2025-12-21 23:22:39 -0500

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Ruwan Thalgahage on 2025-12-21.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
