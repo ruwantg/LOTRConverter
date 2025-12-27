e4c0cd68a51fc07a0fcbe3216fab7edc1524a1dd
Ruwan Thalagaha Gedara <ruwantgs@gmail.com>
2025-12-26 23:11:55 -0500

    feat(ui): add Exchange Info view UI

---
+++ LOTRConverter/ExchangeInfo.swift
@@ -0,0 +1,53 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter
+//
+//  Created by Ruwan Thalgahage on 2025-12-26.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        ZStack {
+            // Background parchment image
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                // Title text
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                    .tracking(3)
+                
+                // Description text
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(.title3)
+                    .padding()
+                
+                // Exchange rates
+                ExchangeRate(leftImage: .goldpiece, text: "1 Gold Piece = 4 Gold Pennies", rightImage: .goldpenny)
+                ExchangeRate(leftImage: .goldpenny, text: "1 Gold Penny = 4 Silver Pieces", rightImage: .silverpiece)
+                ExchangeRate(leftImage: .silverpiece, text: "1 Silver Piece = 4 Silver Pennies", rightImage: .silverpenny)
+                ExchangeRate(leftImage: .goldpenny, text: "1 Silver Penny = 100 Copper Pennies", rightImage: .silverpiece)
+                
+                // Done button
+                Button("Done") {
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown.mix(with: .black, by: 0.2))
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .foregroundStyle(.black)
+        }
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
